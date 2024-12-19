89c68b0865525d0d370b11ebe04e2c0e8902c1d9
Enrique Sosa <enriquesosa.gt@gmail.com>
2024-12-19 01:16:59 -0600

    Working in errors

--- source/thirdparty/glad/_build.cmake
+++ source/thirdparty/glad/_build.cmake
@@ -1,4 +1,4 @@
 # Include includes
-target_include_directories("${CMAKE_CURRENT_SOURCE_DIR}/include")
+include_directories("${CMAKE_CURRENT_SOURCE_DIR}/include")
 
-target_sources(PolyEngineExe PRIVATE "src/glad.c")
+target_sources(PolyEngineExe PRIVATE "${CMAKE_CURRENT_SOURCE_DIR}/src/glad.c")
